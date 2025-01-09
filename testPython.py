dfc101080902d0c65e5974f39462725a335b728d
Faniry1015 <frandriaharimino@yahoo.com>
2025-01-09 14:56:45 +0300

    update

--- testPython.py
+++ testPython.py
@@ -1,15 +1,5 @@
-list_1 = [-1, 2, 6, -10, 50, 100, 4, -20]
-
-classeur1  = {
-    "positif": [],
-    "negatif": []
-}
-def trier(classeur, nombres):
-    for nombre in nombres :
-        if nombre >=0 :
-            classeur["positif"].append(nombre) 
-        else: 
-            classeur["negatif"].append(nombre)
-    return classeur
-
-print(trier(classeur1, list_1))
+#liste compréhension
+prenoms = ["Pierre", "Jean", "Monique"]
+age = [30, 35, 60]
+dico_1 = {k:v for k,v in zip(prenoms, age) if v > 32 } #if ajoute une condition
+print(dico_1)
