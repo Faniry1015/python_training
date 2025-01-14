84c2be4f10fc4f882d37b3f05eb9204395c1aeb7
Faniry1015 <frandriaharimino@yahoo.com>
2025-01-14 06:47:47 +0300

    update

--- testPython.py
+++ testPython.py
@@ -1,5 +1,5 @@
-#liste compréhension
-prenoms = ["Pierre", "Jean", "Monique"]
-age = [30, 35, 60]
-dico_1 = {k:v for k,v in zip(prenoms, age) if v > 32 } #if ajoute une condition
-print(dico_1)
+list_1 = [1, 6, 10, -2]
+name = ["faniry", "elisa","Ando", "ndraina"]
+
+dict_1 = {k:v for k,v in enumerate(name)}
+print(dict_1)
