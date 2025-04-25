4ca2073af45c5591ebd8877f6aa33411cd3e9647
Faniry1015 <frandriaharimino@yahoo.com>
2025-04-25 09:03:13 +0300

    update

--- testPython.py
+++ testPython.py
@@ -1,3 +1,7 @@
-nom = "Faniry"
-for i in range(len(nom)):
-    print(nom[i].capitalize())
+prenoms = ["Pierre", "Jean", "Monique"]
+age = [30, 35, 60]
+dico = {k:v for k,v in enumerate(prenoms)}
+dico_1 = {k:v for k,v in zip(prenoms, age) if v > 32 } #if ajoute une condition
+
+print(dico)
+print(dico_1)
