7d5200006ab130e71b11274852a3988d72dbbefb
Faniry1015 <frandriaharimino@yahoo.com>
2025-12-25 07:12:36 +0300

    update

--- fun_training.py
+++ fun_training.py
@@ -1,7 +1,4 @@
-f = open(r"text.txt", "r", encoding="utf-8")
-f2 = open(r"output.txt", "w", encoding="utf-8")
-for line in f:
-    f2.write(line)
-f.close()
-f2_content = open(r"output.txt", "r", encoding="utf-8").read()
-print(f2_content)
+with open(r'spam.bin', 'bw') as f:
+    for i in range(10):
+        f.write(b'\x3d')
+        
